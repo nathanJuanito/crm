1d58d2f5fe2a8e638994d78474682959c7405ea8
nathanJuanito <154326723+nathanJuanito@users.noreply.github.com>
2025-03-22 15:32:31 +0300

    Update base.sql

--- base.sql
+++ base.sql
@@ -11,3 +11,13 @@ CREATE TABLE taux_alerte(
    taux_alerte DECIMAL(15,2) ,
    PRIMARY KEY(id)
 )ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE depenses(
+   id INT AUTO_INCREMENT,
+   montant DECIMAL(15,2)  ,
+   lead_id INT unsigned NOT NULL,
+   ticket_id INT unsigned NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(lead_id) REFERENCES trigger_lead(lead_id),
+   FOREIGN KEY(ticket_id) REFERENCES trigger_ticket(ticket_id)
+);
